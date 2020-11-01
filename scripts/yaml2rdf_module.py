eb9c7a348e55d4884b4be543b00e71b3ba608ee1
hoijui <hoijui.quaero@gmail.com>
2020-11-01 23:03:59 +0100

    Link to res. RDF from module, and combine the RDF

--- scripts/yaml2rdf_module.py
+++ scripts/yaml2rdf_module.py
@@ -3,12 +3,57 @@ Converts ASKotec training module meta-data
 (from module.yaml) into an RDF/Turtle.
 '''
 
-import re
+import glob
 import os
-import yaml
-import rdflib
 from rdflib.namespace import DC, DCTERMS, DOAP, FOAF, SKOS, OWL, RDF, RDFS, VOID, XMLNS, XSD
+import wget
 from yaml2rdf_shared import *
+from yaml2rdf import convert
+
+KEY_RESOURCE_URL_YAML = 'yaml-url'
+KEY_RESOURCE_URL_RDF = 'rdf-url'
+KEY_RESOURCE_FILE_RDF = 'rdf-file'
+
+def download(url, path):
+    '''
+    Downloads a URL pointing to a file into a local file,
+    pointed to by path.
+    '''
+    print('downloading %s to %s ...' % (url, path))
+    if os.path.exists(path):
+        os.remove(path)
+    wget.download(url, path, None)
+
+def ensure_resource_turtles(yaml_cont, g):
+    '''
+    Either downloads the reosurce RDF files directly,
+    or downloads their YAML version, an dconverts them to RDF afterwards.
+    '''
+
+    res_i = 0
+    for elem in yaml_cont['resources']:
+        if KEY_RESOURCE_URL_YAML in elem:
+            res_yml_url = elem[KEY_RESOURCE_URL_YAML]
+            res_yml_file = os.path.join(os.path.curdir,
+                    'resource_%d.yml' % res_i)
+            res_ttl_file = os.path.join(os.path.curdir,
+                    'resource_%d.ttl' % res_i)
+            res_pre_file = os.path.join(os.path.curdir,
+                    'resource_%d.pref' % res_i)
+            download(res_yml_url, res_yml_file)
+            convert(res_yml_file, res_ttl_file, res_pre_file)
+            yaml_cont[KEY_RESOURCE_FILE_RDF] = res_ttl_file
+        elif KEY_RESOURCE_URL_RDF in elem:
+            res_ttl_url = elem[KEY_RESOURCE_URL_RDF]
+            res_ttl_file = os.path.join(os.path.curdir,
+                    'resource_%d.ttl' % res_i)
+            download(res_ttl_url, res_ttl_file)
+            yaml_cont[KEY_RESOURCE_FILE_RDF] = res_ttl_file
+        else:
+            conv_fail('Resource needs either of %s or %s spezified'
+                    % (KEY_RESOURCE_URL_YAML, KEY_RESOURCE_URL_RDF))
+        res_i = res_i + 1
+
 
 def convert_module_yaml_to_rdf(yaml_cont, g):
     '''
@@ -21,8 +66,15 @@ def convert_module_yaml_to_rdf(yaml_cont, g):
         raise 'The content version is not supported by this converter. Please get a newer version!'
 
     y = yaml_cont['module']
-    pre_path = 'resource'
+    pre_path = 'module'
+
     m_s = ASKM[str2id(y['name'])]
+
+    ensure_resource_turtles(y, g)
+    for res_ttl in glob.glob('resource_*.ttl'):
+        g.parse(res_ttl, format='ttl')
+    for res_s,_,_ in g.triples((None, RDF['type'], ASK['Resource'])):
+        g.add(( m_s, ASK.resource, res_s ))
 
     g.add(( m_s, RDF.type, ASK.Module ))
     g.add(( m_s, RDFS.label, rdf_str(y['name']) ))
@@ -41,9 +93,5 @@ def convert_module_yaml_to_rdf(yaml_cont, g):
     g.add(( m_s, ASK.issues, rdf_str(y['issues']) ))
     g.add(( m_s, ASK.newIssue, rdf_str(y['new-issue']) ))
 
-    for elem in y['resources']:
-        g.add(( m_s, ASKM.resource, rdf_str(elem['url']) ))
-
     conv_authors(y, g, m_s)
     conv_licenses(y, g, m_s)
-
